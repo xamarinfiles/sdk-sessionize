4cd71a7b82e224b08dabfd5ea4c27d3f9e5a05e2
Andy Lech <github@andylech.com>
2020-02-21 14:56:09 -0500

    Update filename and URL handling for greater flexibility

--- SessionizeApi/Shared/SessionizeApi.Samples/Filenames.cs
+++ SessionizeApi/Shared/SessionizeApi.Samples/Filenames.cs
@@ -1,24 +1,98 @@
-﻿namespace SessionizeApi.Samples
+﻿using System;
+
+namespace SessionizeApi.Samples
 {
-    internal static class Filenames
+    internal struct Filenames
     {
-        #region API
+        private const string AllDataUrlFormat =
+            "https://sessionize.com/api/v2/{0}/view/All";
 
-        internal static readonly string SessionizeAllData = "Sessionize\\All.json";
+        private const string SmartGridUrlFormat =
+            "https://sessionize.com/api/v2/{0}/view/GridSmart";
 
-        internal static readonly string SessionizeGridSmart = "Sessionize\\GridSmart.json";
+        private const string SessionsUrlFormat =
+            "https://sessionize.com/api/v2/{0}/view/Sessions";
 
-        internal static readonly string SessionizeSessions = "Sessionize\\Sessions.json";
+        private const string SpeakersUrlFormat =
+            "https://sessionize.com/api/v2/{0}/view/Speakers";
 
-        internal static readonly string SessionizeSpeakers = "Sessionize\\Speakers.json";
+        private const string SpeakerWallUrlFormat =
+            "https://sessionize.com/api/v2/{0}/view/SpeakerWall";
 
-        internal static readonly string SessionizeSpeakerWall = "Sessionize\\SpeakerWall.json";
+        #region API Samples
+
+        internal struct Sessionize
+        {
+            internal struct Local
+            {
+                internal const string AllData =
+                    "Sessionize\\All.json";
+
+                internal const string SmartGrid =
+                    "Sessionize\\GridSmart.json";
+
+                internal const string Sessions =
+                    "Sessionize\\Sessions.json";
+
+                internal const string Speakers =
+                    "Sessionize\\Speakers.json";
+
+                internal const string SpeakerWall =
+                    "Sessionize\\SpeakerWall.json";
+            }
+
+            internal struct Web
+            {
+                internal const string _sampleEvent = "jl4ktls0";
+
+                internal static readonly string AllData =
+                    string.Format(AllDataUrlFormat, _sampleEvent);
+
+                internal static readonly string Schedule =
+                    string.Format(SmartGridUrlFormat, _sampleEvent);
+
+                internal static readonly string Sessions =
+                    string.Format(SessionsUrlFormat, _sampleEvent);
+
+                internal static readonly string Speakers =
+                    string.Format(SpeakersUrlFormat, _sampleEvent);
+
+                internal static readonly string SpeakerWall =
+                    string.Format(SpeakerWallUrlFormat, _sampleEvent);
+            }
+        }
 
         #endregion
 
         #region Events
 
-        internal static readonly string OrlandoCodeCamp2020AllData = "Events\\OrlandoCodeCamp\\2020-All.json";
+        internal struct Event
+        {
+            internal struct Local
+            {
+                public Local(string eventFolder, int year)
+                {
+                    EventFolder = eventFolder;
+                    Year = year;
+                }
+
+                internal string EventFolder { get; private set; }
+
+                internal int Year { get; private set; }
+
+                internal string AllData =>
+                    $"Events\\{EventFolder}\\{Year}\\All.json";
+            }
+
+            internal struct Web
+            {
+                public Web(string eventId) => EventId = eventId;
+
+                internal string EventId { get; private set; }
+
+                internal Uri AllData => new Uri(string.Format(AllDataUrlFormat, EventId));
+            }
+        }
 
         #endregion
     }
