3c2f68bf38bcf167a300dacc7dd20d1f4743b599
Andy Lech <github@andylech.com>
2023-02-19 22:13:49 -0500

    Add new Trello exporter for session scheduling board

---
+++ Schedule/SessionizeApi.Trello.Tests.Smoke/Program.cs
@@ -0,0 +1,117 @@
+﻿using SessionizeApi.Importer;
+using SessionizeApi.Importer.Logger;
+using SessionizeApi.Importer.Models;
+using System.Diagnostics.CodeAnalysis;
+using static SessionizeApi.Importer.SessionizeImporter;
+#pragma warning disable IDE0051
+
+namespace SessionizeApi.Trello.Tests.Smoke
+{
+    [SuppressMessage("ReSharper", "UnusedMember.Local")]
+    [SuppressMessage("ReSharper", "UnusedMethodReturnValue.Local")]
+    internal static class Program
+    {
+        // TODO Centralize these
+        #region Sessionize Fields
+
+        private const string SessionizeSampleId = "** TODO **";
+
+        private const string Occ2023Id = "** TODO **";
+
+        #endregion
+
+        #region Trello Fields
+
+        private const string TrelloApiKey = "** TODO **";
+
+        private const string TrelloApiToken = "** TODO **";
+
+        private const string SessionizeBoardId = "** TODO **"; // Sessionize Sample
+
+        private const string TrelloBoardId = SessionizeBoardId;
+
+        #endregion
+
+        #region Services
+
+        //private static CsvExporter CsvExporter { get; set; }
+
+        private static LoggingService LoggingService { get; } = new();
+
+        private static SessionizeImporter SessionizeImporter { get; } =
+            new(LoggingService);
+
+        private static SessionizeLogger SessionizeLogger { get; } = new(LoggingService);
+
+        private static TrelloExporter TrelloExporter { get; set; } =
+            new(LoggingService, TrelloApiKey, TrelloApiToken);
+
+        #endregion
+
+        #region Setup
+
+        internal static async Task Main()
+        {
+            try
+            {
+                var @event = LoadAndPrintEventFromUrl(SessionizeSampleId);
+
+                // var @event = LoadAndPrintEventFromUrl(Occ2023Id);
+
+                if (@event is null)
+                    return;
+
+                await ExportEventSessions(@event, TrelloBoardId);
+            }
+            catch (Exception exception)
+            {
+                LoggingService.LogExceptionRouter(exception);
+            }
+        }
+
+        #endregion
+
+        #region Private - Export to Trelo
+
+        private static async Task ExportEventSessions(Event @event, string boardId)
+        {
+            try
+            {
+                await TrelloExporter.ConnectToTrelloAndLoadBoard(@event, boardId);
+            }
+            catch (Exception exception)
+            {
+                LoggingService.LogExceptionRouter(exception);
+            }
+        }
+
+        #endregion
+
+        // TODO Consolidate with SessionizeApi.Importer.Tests.Smoke
+        #region Private - Import from Sessionize
+
+        // TODO Add file equivalent
+
+        private static Event? LoadAndPrintEventFromUrl(string eventId)
+        {
+            var webUrl = PopulateUrl(eventId);
+            var webEvent =
+                Task.Run(() => SessionizeImporter.ImportAllDataFromUri(webUrl))
+                    .GetAwaiter().GetResult();
+
+            SessionizeLogger.PrintEvent(webEvent);
+
+            return webEvent;
+        }
+
+        private static Uri PopulateUrl(string sessionizeId)
+        {
+            var sessionizeUrlStr = string.Format(SessionizeUrlFormat, sessionizeId);
+            var sessionizeUrl = new Uri(sessionizeUrlStr);
+
+            return sessionizeUrl;
+        }
+
+        #endregion
+    }
+}
---
+++ Schedule/SessionizeApi.Trello/TrelloExporter.cs
@@ -0,0 +1,258 @@
+﻿using Manatee.Trello;
+using SessionizeApi.Importer.Logger;
+using SessionizeApi.Importer.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Threading.Tasks;
+using static Manatee.Trello.LabelColor;
+using TrelloIList = Manatee.Trello.IList;
+
+namespace SessionizeApi.Trello
+{
+    [SuppressMessage("ReSharper", "ArrangeObjectCreationWhenTypeEvident")]
+    public class TrelloExporter
+    {
+        #region Constants
+
+        private const string AcceptedListName = "Accepted Sessions";
+
+        private const string UnselectedListName = "Unselected Sessions";
+
+        #endregion
+
+        #region Services
+
+        private static LoggingService LoggingService { get; set; }
+
+        #endregion
+
+        #region Sessionize Properies
+
+        private Event SessionizeEvent { get; set; }
+
+        #endregion
+
+        #region Trello Properties
+
+        private static string ApiKey { get; set; }
+
+        private static string ApiToken { get; set; }
+
+        private static LabelColor DefaultLabelColor { get; set; }
+
+        private readonly TrelloFactory _trelloFactory = new TrelloFactory();
+
+        private string BoardId { get; set; }
+
+        private IBoard Board { get; set; }
+
+        #endregion
+
+        #region Trello Methods
+
+        public TrelloExporter(LoggingService loggingService, string apiKey,
+            string apiToken, LabelColor defaultLabelColor = Black)
+        {
+            LoggingService = loggingService;
+            ApiKey = apiKey;
+            ApiToken = apiToken;
+            DefaultLabelColor = defaultLabelColor;
+        }
+
+        public async Task ConnectToTrelloAndLoadBoard(Event @event, string trelloBoardId)
+        {
+            SessionizeEvent = @event;
+            BoardId = trelloBoardId;
+
+            // TODO BuildSessionizeCategoryDictionary();
+
+            SetupTrelloUserCredentials();
+
+            Board = await ConnectToTrelloBoard();
+
+            await LoadTrelloBoard();
+        }
+
+        private static void SetupTrelloUserCredentials()
+        {
+            try
+            {
+                TrelloAuthorization.Default.AppKey = ApiKey;
+                TrelloAuthorization.Default.UserToken = ApiToken;
+            }
+            catch (Exception exception)
+            {
+                LoggingService.LogExceptionRouter(exception);
+            }
+        }
+
+        private async Task<IBoard> ConnectToTrelloBoard()
+        {
+            try
+            {
+                var board = _trelloFactory.Board(BoardId);
+                await board.Refresh();
+
+                return board;
+            }
+            catch (Exception exception)
+            {
+                LoggingService.LogExceptionRouter(exception);
+
+                return null;
+            }
+        }
+        private async Task LoadTrelloBoard()
+        {
+            try
+            {
+                // TODO Group a user and all their sessions, sorted by first submission
+                var sortedSpeakers =
+                    SessionizeEvent.Speakers.OrderBy(speaker => speaker.FullName).ToList();
+
+                var acceptedList = await LoadTrelloList(AcceptedListName);
+                var unselectedList = await LoadTrelloList(UnselectedListName);
+
+                for (var speakerIndex = 0;
+                     speakerIndex < SessionizeEvent.Speakers.Length;
+                     speakerIndex++)
+                {
+                    var speaker = sortedSpeakers[speakerIndex];
+
+                    LoggingService.LogHeader("{0}: {1}", speakerIndex,
+                        speaker.LogDisplayShort);
+
+                    foreach (var sessionIdAndName in speaker.SessionIdsAndNames)
+                    {
+                        LoggingService.LogSubheader("{0}: {1}", speakerIndex,
+                            sessionIdAndName.LogDisplayShort);
+
+                        var session =
+                            SessionizeEvent.SessionDictionary[sessionIdAndName.Id];
+                        if (session is null)
+                        {
+                            continue;
+                        }
+
+                        // HACK OCC 2023 only categorized accepted sessions
+                        var categories = session.CategoryIdsAndNames;
+                        await LoadTrelloCard(
+                            categories?.Count() < 1 ? unselectedList : acceptedList,
+                            session);
+                    }
+
+                    LoggingService.LogBlankLine();
+                }
+
+                await acceptedList.Refresh();
+                var acceptedCount = acceptedList.Cards.Count();
+                LoggingService.LogValue("Accepted Sessions", acceptedCount.ToString());
+
+                await unselectedList.Refresh();
+                var unselectedCount = unselectedList.Cards.Count();
+                LoggingService.LogValue("Unselected Sessions", unselectedCount.ToString());
+
+                var totalCount = acceptedCount + unselectedCount;
+                LoggingService.LogValue("Total Sessions", totalCount.ToString());
+            }
+            catch (Exception exception)
+            {
+                LoggingService.LogExceptionRouter(exception);
+            }
+        }
+
+        private async Task<TrelloIList> LoadTrelloList(string listName)
+        {
+            switch (Board.Lists.FirstOrDefault(list => list.Name.Contains(listName)))
+            {
+                case { } oldList:
+                    return oldList;
+                default:
+                {
+                    var newList = await Board.Lists.Add(listName);
+
+                    return newList;
+                }
+            }
+        }
+
+        private async Task<ILabel> LoadTrelloLabel(string labelName)
+        {
+            switch (Board.Labels.FirstOrDefault(label => label.Name.Contains(labelName)))
+            {
+                case { } oldLabel:
+                    return oldLabel;
+                default:
+                {
+                    // TODO Support more than one color? Random? Sequential?
+                    var newLabel = await Board.Labels.Add(labelName, DefaultLabelColor);
+
+                    return newLabel;
+                }
+            }
+        }
+
+        private async Task LoadTrelloCard(TrelloIList trelloList, Session session)
+        {
+            // TEMP
+            LoggingService.LogValue(trelloList.Name, session.Title);
+
+            try
+            {
+                var cardName =
+                    $"{session.Id} - {session.SpeakerNames} - {session.Title}";
+                var cardDescription = session.Description;
+
+                // TEMP
+                LoggingService.LogValue("Card Name", cardName);
+                LoggingService.LogValue("Card Description", cardDescription);
+
+                var sessionCategories =
+                    session.CategoryIdsAndNames
+                        .Select(category => category.Name)
+                        .ToList();
+
+                var cardLabels = new List<ILabel>();
+                foreach (var category in sessionCategories)
+                {
+                    var label = await LoadTrelloLabel(category);
+
+                    LoggingService.LogValue(category, label.Id);
+
+                    cardLabels.Add(label);
+                }
+
+                if (cardLabels.Count > 0)
+                    // TEMP
+                    LoggingService.LogValue("Card Labels",
+                        string.Join(", ",
+                            cardLabels.Select(label => label.Name).ToList()));
+
+                await trelloList.Refresh();
+
+                // TODO Figure out why this is duplicating first sample session
+                if (trelloList.Cards
+                    .Any(card => card.Name.StartsWith(session.Id.ToString())))
+                {
+                    LoggingService.LogWarning("DUPLICATE");
+
+                    return;
+                }
+
+                await trelloList.Cards.Add(
+                    name: cardName,
+                    description: cardDescription,
+                    labels: cardLabels
+                );
+            }
+            catch (Exception exception)
+            {
+                LoggingService.LogExceptionRouter(exception);
+            }
+        }
+
+        #endregion
+    }
+}
