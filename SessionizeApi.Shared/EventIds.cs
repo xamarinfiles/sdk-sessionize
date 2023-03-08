163660ca3a5bac56d00a2a497e29ef48dae3c794
Andy Lech <github@andylech.com>
2023-03-08 14:30:06 -0500

    Add Shared project with event ids and common test code

---
+++ SessionizeApi.Shared/EventIds.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SessionizeApi.Shared
+{
+    internal abstract class EventIds
+    {
+        #region Fields
+
+        internal const string SessionizeSampleId = "jl4ktls0";
+
+        #endregion
+    }
+}
---
+++ SessionizeApi.Shared/EventTester.cs
@@ -0,0 +1,60 @@
+﻿using SessionizeApi.Importer;
+using SessionizeApi.Importer.Logger;
+using SessionizeApi.Importer.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace SessionizeApi.Shared
+{
+    internal static class EventTester
+    {
+        #region Fields
+
+        private const string SessionizeUrlFormat =
+            "https://sessionize.com/api/v2/{0}/view/All";
+
+        #endregion
+
+        #region Methods
+
+        internal static Event? LoadEventFromFileAndPrint(string filename,
+            EventImporter importerService, EventPrinter printerService,
+            bool printEvent = true)
+        {
+            var fileEvent =
+                Task.Run(() =>
+                        importerService.ImportAllDataFromFile(filename))
+                    .GetAwaiter().GetResult();
+
+            printerService.PrintEvent(fileEvent, printEvent);
+
+            return fileEvent;
+        }
+
+        internal static Event? LoadEventFromUrlAndPrint(string eventId,
+            EventImporter importerService, EventPrinter printerService,
+            bool printEvent = true)
+        {
+            var webUrl = PopulateUrl(eventId);
+            var webEvent =
+                Task.Run(() =>
+                        importerService.ImportAllDataFromUri(webUrl))
+                    .GetAwaiter().GetResult();
+
+            printerService.PrintEvent(webEvent, printEvent);
+
+            return webEvent;
+        }
+
+        private static Uri PopulateUrl(string eventId,
+            string urlFormat = SessionizeUrlFormat)
+        {
+            var sessionizeUrlStr = string.Format(urlFormat, eventId);
+            var sessionizeUrl = new Uri(sessionizeUrlStr);
+
+            return sessionizeUrl;
+        }
+
+        #endregion
+    }
+}
